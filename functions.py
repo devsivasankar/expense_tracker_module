d29a84894e88b9585da7bb14a61c21e60a50c434
devsivasankar <mypostbox.sivasankar@gmail.com>
2025-05-15 23:50:56 +0530

    expense tracker with module

---
+++ functions.py
@@ -0,0 +1,43 @@
+database = []
+data = {}
+total_amount = 0
+id = 0
+
+def Get_Enpense():
+    while True:
+        global total_amount
+        global id
+        id = id + 1
+        date = input('Enter Date(yyyy/mm/dd):  ')
+        description = input('Enter Item Purchased:  ')
+        try:
+            amount = float(input('Enter Amount Spent:  '))
+            if amount < 0:
+                print('Enter Valid Amount')
+            else:
+                database.append({'id':id,'date':date, 'description':description, 'amount':amount})
+                total_amount = amount + total_amount
+                break       
+        except:
+            print('enter valid amount')
+
+
+def View_Expense_List():
+    if not database:
+        print('No Datas Available Here')
+    else:    
+        print('id\t\tDate\t\tDescription\t\tAmount')
+        print('-'*70)
+        for datas in database:
+            print(f"{datas['id']:<15}{datas['date']:<20}{datas['description']:<20}{datas['amount']}")
+        print(f"{total_amount:<50}")
+
+
+
+
+def Delete_Expense():
+    pass
+
+
+def Download_Expense():
+    pass
---
+++ main.py
@@ -0,0 +1,21 @@
+import functions
+
+
+
+print('Monthly Expense Tracker')
+while True:
+    print('1.Enter Expense\n2.View Expense List\n3.Delete Expense\n4.Download Expense List\n5.Exit')
+    userinput = input('Enter Your Choice:  ')
+    if userinput == '1':
+        functions.Get_Enpense()
+    elif userinput == '2':
+        functions.View_Expense_List()
+    elif userinput == '3':
+        functions.Delete_Expense()
+    elif userinput == '4':
+        functions.Download_Expense()
+    elif userinput == '5':
+        print('you are logged out..thank you')
+        quit()
+    else:
+        print('invalid input')
